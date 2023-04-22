466f65b912a7336a1a46070c7bc5318140833d05
c-mckeon <conor.mckeon4@gmail.com>
2023-04-22 13:34:44 +0200

    update

--- script.js
+++ script.js
@@ -64,8 +64,3 @@ presleuko.onclick = function() {
 };
 
 
-window.addEventListener('load', function() {
-  var container = document.querySelector('.container');
-  var sidebar = document.querySelector('.sidebar');
-  sidebar.style.height = container.offsetHeight + 'px';
-});
--- script2.js
+++
@@ -1,6 +0,0 @@
-
-window.addEventListener('load', function() {
-  var container = document.querySelector('.container');
-  var sidebar = document.querySelector('.sidebar');
-  sidebar.style.height = container.offsetHeight + 'px';
-});
